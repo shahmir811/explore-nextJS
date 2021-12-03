e8d7ad4b466b9594b4a5fe9d882d9595c053b0ae
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-12-03 16:15:28 +0500

    Added MainHeader, style some components

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,12 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+
 const EventsPage = () => {
+	const events = getAllEvents();
+
 	return (
 		<div>
-			<h1>All events</h1>
+			<EventList items={events} />
 		</div>
 	);
 };
