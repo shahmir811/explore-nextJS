2ef6bc391e6828b242c4f07e6d3842cfffddd09f
Shahmir Khan Jadoon <sjaoon@ssidecisions.com>
2021-12-03 15:36:21 +0500

    Initial commit

---
+++ components/events/EventList.js
@@ -0,0 +1,17 @@
+import EventItem from './EventItem';
+
+import classes from './EventList.module.css';
+
+const EventList = props => {
+	const { items } = props;
+
+	return (
+		<ul className={classes.list}>
+			{items.map(event => (
+				<EventItem key={event.id} event={event} />
+			))}
+		</ul>
+	);
+};
+
+export default EventList;
